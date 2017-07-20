897ce29868121fa0fae27a83e675c6eb2a91151a
Gilga <gilgatx@gmail.com>
2017-07-20 23:46:28 +0200

    added board array + create board image

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,10 @@
+import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.imageio.ImageIO;
 
 import lenz.htw.kipifub.ColorChange;
 import lenz.htw.kipifub.net.NetworkClient;
@@ -19,6 +24,7 @@ public class Client extends Thread {
     
     ThreadLocalRandom tlr = null;
     
+    int[][] board = new int[1024][1024];
     int color = 0;
     int[] radius = new int[]{0,0,0}; // radius
     float[] ang = new float[]{0,0,0};
@@ -33,22 +39,29 @@ public class Client extends Thread {
 
     boolean[] infinity = new boolean[]{false,false,false};
     
-    void moveDirection(int i, Position d){
-
+    void move(int i) {
+    	Position p = pos[i];
+    	Position t = targetPos[i];
+    	Position d = new Position(t.x-p.x, t.y-p.y);
+    	
 		if(!Position.equals(oldDir[i], d)){
 			oldDir[i]=d;
 			networkClient.setMoveDirection(i, d.x, d.y);
-    		System.out.printf("(%d|%d){%d} [%f,%f] -> [%f,%f]\n", myID, i, radius[i], pos[i].x, pos[i].y, d.x, d.y);
-			//System.out.printf("[%d.%d] (%f,%f)\n", myID, i, nd.x, nd.y);
+    		//System.out.printf("(%d|%d){%d} [%f,%f] -> [%f,%f]\n", myID, i, radius[i], pos[i].x, pos[i].y, d.x, d.y);
 		}
     }
-    
-    void moveTo(int i, Position p) {
-    	moveDirection(i, new Position(p.x-pos[i].x, p.y-pos[i].y));
-    }
      
-    boolean isWalkable(Position p) {
+    
+    boolean isValid(Position p) {
+    	if(p.x<0||p.y<0||p.x>1023||p.y>1023) return false;
+		//return board[(int)p.x][(int)p.y] != 0x0; // working incorrectly
 		return networkClient.isWalkable((int)p.x, (int)p.y);
+    }
+
+    int getBoard(Position p){
+    	if(!isValid(p)) return 0x0;
+    	//return board[(int)p.y][(int)p.x]; // working incorrectly
+    	return 0xFF000000 | networkClient.getBoard((int)p.x, (int)p.y);
     }
     
     boolean canMoveTo(int i, Position t, float step) {
@@ -61,12 +74,12 @@ public class Client extends Thread {
    	
     	step=(radius[i]/2+1);
     	
-    	if(!isWalkable(Position.add(p, Position.mul(d,new Position(step))))) return false;
+    	if(!isValid(Position.add(p, Position.mul(d,new Position(step))))) return false;
 
     	for(float j=0;j<step;j++) //(dist/step)
     	{
     		p=Position.add(p, d);
-    		if(!isWalkable(p)) return false;
+    		if(!isValid(p)) return false;
     	}
 
     	return true;
@@ -98,9 +111,9 @@ public class Client extends Thread {
     
     boolean boardHasWhite() {
     	Position p = new Position();
-    	for(p.y=0;p.y<1000;p.y++){
-        	for(p.x=0;p.x<1000;p.x++){
-        		if(getRGB(p) == 0xFFFFFF) //isWalkable(p) &&
+    	for(p.y=0;p.y<board.length;p.y++){
+        	for(p.x=0;p.x<board[0].length;p.x++){
+        		if(getBoard(p) == 0xFFFFFFFF) //isWalkable(p) &&
         			return true;
         	}
     	}
@@ -132,7 +145,7 @@ public class Client extends Thread {
      	float z = radius[i];
      	float sz = 0;
      	Position p = pos[i];
-     	int rgb = getRGB(t);
+     	int rgb = getBoard(t);
      	float sr = 0;
      	
      	if(infinity[i] || !colorIsValid(i,rgb) || dist<=7.5f)
@@ -144,9 +157,9 @@ public class Client extends Thread {
 	    			a = old_ang + sr + r;
 		    		d = Position.angToPosition(a);
 		    		t = p.getNext(i, d, sz);
-	    			rgb = getRGB(t);
+	    			rgb = getBoard(t);
 	    			
-	    			if(!isWalkable(t)||ColorPixel.isColorDominant(rgb,color)) continue;
+	    			if(!isValid(t)||ColorPixel.isColorDominant(rgb,color)) continue;
 
 		    		if(colorIsValid(i,rgb))
 		    		{
@@ -178,7 +191,7 @@ public class Client extends Thread {
 		for(int r=0;r<360;r++){
 			a=an+r;
 			for(s=1;s<max;s++) {
-				if(!isWalkable(p.getNext(i, Position.angToPosition(a), s))) {
+				if(!isValid(p.getNext(i, Position.angToPosition(a), s))) {
 					if(s<ts) { found=true; ts = s; tr = a;}
 					break;
 				}
@@ -195,7 +208,7 @@ public class Client extends Thread {
     
     Position[] waitPos = new Position[]{new Position(),new Position(),new Position()};
     
-    void chooseAndMove(int i) {
+    void setNewTarget(int i) {
     	searchFreeSpot(i);
 
     	if(wait[i]>=3) {
@@ -211,15 +224,61 @@ public class Client extends Thread {
     	else {
     		wait[i]++;
     	}
-
-    	moveTo(i, targetPos[i]);
-    }
-    
-    int getRGB(Position p){
-    	return networkClient.getBoard((int)p.x, (int)p.y);
     }
     
     void update(int i) {}
+
+    boolean updateBoardFirstTime = true;
+    
+    public void updateBoard() {
+    	int width = board[0].length;
+    	int height = board.length;
+    	
+    	int rgb = 0;
+    	int x = 0;
+    	int y = 0;
+    	for(int i = 0; i<width*height; i++) {
+    		if(updateBoardFirstTime || board[y][x] != 0){
+	    		rgb = networkClient.getBoard(x,y);
+	    		if(!updateBoardFirstTime || rgb!=0) {
+	    			board[y][x] = 0xFF000000 | rgb;
+	    		}
+    		}
+    		x++;
+    		if(x>=width) { x=0; y++; }
+    	}
+    	
+    	if(updateBoardFirstTime) updateBoardFirstTime=false;
+    }
+    
+    public void generatePicture()
+    {
+    	int width = board[0].length;
+    	int height = board.length;
+    	BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+    	int x = 0;
+    	int y = 0;
+    	int[] buffer = new int[width*height];
+    	for(int i = 0; i<buffer.length; i++) {
+    		buffer[i] =  board[y][x];
+    		x++;
+    		if(x>=width) { x=0; y++; }
+    	}
+    	
+    	image.setRGB(0,0,width,height,buffer, 0, width);
+    	
+		try {
+			File file = new File(myID + "_board.png");
+			ImageIO.write(image, "png", file);
+			
+		    Desktop dt = Desktop.getDesktop();
+		    dt.open(file);
+		    
+		} catch (Exception e) {
+			System.err.println("Fehler: Konnte Boardbild nicht speichern!");
+		}
+    }
  
     public void run() {
     	tlr = ThreadLocalRandom.current();
@@ -227,7 +286,10 @@ public class Client extends Thread {
 		networkClient = new NetworkClient(host, name);
 		myID = networkClient.getMyPlayerNumber();
 		System.out.printf("Player: %d started\n", myID);
-		 
+
+		updateBoard();
+		//if(myID == 0) generatePicture();
+		
 		// radius
 		radius[0] = networkClient.getInfluenceRadiusForBot(0);
 		radius[1] = networkClient.getInfluenceRadiusForBot(1);
@@ -241,7 +303,7 @@ public class Client extends Thread {
 		
 		boolean foundStartPos = false;
 		ColorChange colorChange = new ColorChange();
-		
+		int g = 0;
 		while(true){
 
 			while ((colorChange = networkClient.pullNextColorChange()) != null) {
@@ -261,7 +323,7 @@ public class Client extends Thread {
 			    		if(bot == 0) {
 
 			    			if(!foundStartPos){
-			    				color = ColorPixel.getDominantColor(getRGB(p));
+			    				color = ColorPixel.getDominantColor(getBoard(p));
 				    			System.out.printf("%d: %s color\n", myID, ColorPixel.getColorStr(color));
 			    			}
 			    			foundStartPos = true;
@@ -271,9 +333,20 @@ public class Client extends Thread {
 			}
 			
 			if(foundStartPos) {
-				chooseAndMove(0);
-				chooseAndMove(1);
-				chooseAndMove(2);
+				updateBoard();
+				
+				if(myID == 0 && g==500) {
+					generatePicture();
+				}
+				g++;
+				
+				setNewTarget(0);
+				setNewTarget(1);
+				setNewTarget(2);
+				
+				move(0);
+				move(1);
+				move(2);
 			}
 			
 			try {
--- src/ColorPixel.java
+++ src/ColorPixel.java
@@ -4,47 +4,48 @@ public class ColorPixel {
 		int b = rgb & 0xFF;
 		int g = (rgb >> 8) & 0xFF;
 		int r = (rgb >> 16) & 0xFF;
-    	return new int[]{r,g,b};
+		int a = (rgb >> 24) & 0xFF;
+    	return new int[]{r,g,b,a};
     }
     
     static public int getColorStrength(int rgb, int color){
 		int[] c = splitColor(rgb);
-		return color == 0xFF ? c[2] : (color == 0xFF00 ? c[1] : c[0]);
+		return color == 0xFF0000FF ? c[2] : (color == 0xFF00FF00 ? c[1] : c[0]);
     }
     
-    static public boolean isColorNotDominantOrWhite(int rgb, int color){
-    	return rgb == 0xFFFFFF || !isColorDominant(rgb, color);
+    static public boolean isColorNotDominantOrWhite(int rgba, int color){
+    	return rgba == 0xFFFFFFFF || !isColorDominant(rgba, color);
     }
     
-    static public boolean isColorDominant(int rgb, int color){
-    	return color == getDominantColor(rgb);
+    static public boolean isColorDominant(int rgba, int color){
+    	return color == getDominantColor(rgba);
     }
     
     static public float getBrightness(int color)
     {
-		int[] rgb = splitColor(color);
+		int[] rgba = splitColor(color);
 		//int c = ((r>b && r>g)?r:((g>=r && g>=b)?g:((b>=r && b>=g)?b:b)));
-		float c = (rgb[0] * 0.2126f + rgb[1] * 0.7152f + rgb[2] * 0.0722f) / 0xFF;
+		float c = (rgba[0] * 0.2126f + rgba[1] * 0.7152f + rgba[2] * 0.0722f) / 0xFF;
 		return c;
     }
 
     static public int getColorStrength(int color) {
     	int c = getDominantColor(color);
-    	int[] rgb = splitColor(color);
-		int b = rgb[2];
-		int g = rgb[1];
-		int r = rgb[0];
-    	return color == c ? c == 0xFF0000 ? r : (c == 0xFF00 ? g : b) : 0;
+    	int[] rgba = splitColor(color);
+		int b = rgba[2];
+		int g = rgba[1];
+		int r = rgba[0];
+    	return color == c ? c == 0xFFFF0000 ? r : (c == 0xFF00FF00 ? g : b) : 0;
     }
     
     static public int getDominantColor(int color) {
 		int b = color & 255;
 		int g = (color >> 8) & 255;
 		int r = (color >> 16) & 255;
-    	return (r>b && r>g)?0xFF0000:((g>r && g>b)?0xFF00:((b>r && b>g)?0xFF:0));
+    	return (r>b && r>g)?0xFFFF0000:((g>r && g>b)?0xFF00FF00:((b>r && b>g)?0xFF0000FF:0));
     }
     
     static public String getColorStr(int color) {
-    	return (color==0xFF)?"blue":((color==0xFF00)?"green":((color==0xFF0000)?"red":"?"));
+    	return (color==0xFF0000FF)?"blue":((color==0xFF00FF00)?"green":((color==0xFFFF0000)?"red":"?"));
     }
 }
